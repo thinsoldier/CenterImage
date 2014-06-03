88b52bf6f466db9c14897e98dd500283e2915203
thinsoldier <thinsoldier@thinsoldier.com>
2014-06-03 17:48:54 -0400

    Original code used on a few old projects.

---
+++ centerimage.js
@@ -0,0 +1,34 @@
+// centers an img tag relative to its parent
+// the parent MUST have a fixed width and height
+// so in the case of 
+// div a img {}
+// ensure the <a> height is manually defined
+// instead of being determined by the img it contains.
+function centerImage(image)
+{
+	var area = image.up();
+	
+	var $areaW = area.getWidth();
+	var $areaH = area.getHeight();
+	
+	var $imageWidth = image.width;
+	var $imageHeight = image.height;
+
+	var $left = 0;
+	if( $areaW < $imageWidth )
+	{
+		var $left = $areaW - $imageWidth;
+		$left = Math.round($left / 2);
+	}
+	
+	var $top = 0;
+	if($areaH < $imageHeight )
+	{
+		var $top = $areaH - $imageHeight;	
+		$top = Math.round($top /2);
+	}
+		
+	image.style.position = 'relative';
+	image.style.top = $top + 'px';
+	image.style.left = $left + 'px';
+}
